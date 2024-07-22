178f516bcde2fac77d67b60cb0ef2ce1b1230d1a
valentinefleith <valentine.fleith@gmail.com>
2024-07-22 23:19:34 +0200

    add some structure + draw rectangle

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,52 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::WindowCanvas;
+use sdl2::video::Window;
 use std::time::Duration;
 
-fn main() {
+const DOT_SIZE_IN_PXS: u32 = 20;
+
+pub struct Point(pub i32, pub i32);
+
+pub struct Renderer {
+    canvas: WindowCanvas,
+}
+
+impl Renderer {
+    pub fn new(window: Window) -> Result<Renderer, String> {
+        let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+        Ok(Renderer { canvas })
+    }
+
+    fn draw_dot(&mut self, point: &Point) -> Result<(), String> {
+        let Point(x, y) = point;
+        self.canvas.fill_rect(Rect::new(
+            x * DOT_SIZE_IN_PXS as i32,
+            y * DOT_SIZE_IN_PXS as i32,
+            DOT_SIZE_IN_PXS,
+            DOT_SIZE_IN_PXS,
+        ))?;
+        Ok(())
+    }
+
+    pub fn draw(&mut self) -> Result<(), String> {
+        self.canvas.set_draw_color(Color::BLACK);
+        self.canvas.clear();
+
+        self.canvas.set_draw_color(Color::GREEN);
+        self.draw_dot(&Point(3, 1))?;
+        self.draw_dot(&Point(2, 1))?;
+        self.draw_dot(&Point(1, 1))?;
+
+        self.canvas.present();
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -13,30 +56,29 @@ fn main() {
         .window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
-        .unwrap();
+        .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let mut renderer = Renderer::new(window)?;
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
         i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        renderer.draw()?;
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => {
+                    println!("ESC PRESSED");
+                    break 'running;
+                }
                 _ => {}
             }
         }
+        ::std::thread::sleep(Duration::new(1, 1_000_000_000u32 / 60));
     }
-    canvas.present();
-    //::std::thread::sleep(Duration::new(1, 1_000_000_000u32 / 60));
+    Ok(())
 }
