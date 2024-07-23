809846459ff0057f6c62d3c303b22d852f88f991
valentinefleith <valentine.fleith@gmail.com>
2024-07-23 23:51:10 +0200

    remove useless commented code in main

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,7 @@ use sdl2::keyboard::Keycode;
 use std::time::Duration;
 
 fn main() -> Result<(), String> {
-    //let sdl_context = sdl2::init().unwrap();
-    //let video_subsystem = sdl_context.video().unwrap();
-
-    //let window = video_subsystem
-    //.window("rust-sdl2 demo", 800, 600)
-    //.position_centered()
-    //.build()
-    //.map_err(|e| e.to_string())?;
-
-    //let mut renderer = Renderer::new(window)?;
-    //let mut context = Context::new();
     let mut graphic_window = GraphicWindow::new("hello-rustsdl2", 800, 600)?;
     graphic_window.run()?;
-    //let mut event_pump = graphic_window.sdl_context.event_pump()?;
-    //let mut i = 0;
-    //'running: loop {
-    //i = (i + 1) % 255;
-    //graphic_window.renderer.draw(&context)?;
-    //for event in event_pump.poll_iter() {
-    //match event {
-    //Event::Quit { .. }
-    //| Event::KeyDown {
-    //keycode: Some(Keycode::Escape),
-    //..
-    //} => {
-    //println!("ESC PRESSED");
-    //break 'running;
-    //}
-    //_ => {}
-    //}
-    //}
-    //::std::thread::sleep(Duration::new(1, 1_000_000_000u32 / 30));
-    //}
     Ok(())
 }
